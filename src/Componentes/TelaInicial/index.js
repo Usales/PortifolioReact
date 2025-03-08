d65ebb93a6927ac6e36b623b1996bbb214e9d299
GabrielSales <104046747+Usales@users.noreply.github.com>
2025-03-08 12:18:29 -0300

    Corrige problemas de acessibilidade nos links de idioma

--- src/Componentes/TelaInicial/index.js
+++ src/Componentes/TelaInicial/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './TelaInicial.css';
 
 const TelaInicial = ({ language, handlePortugueseClick, handleEnglishClick, handleJapaneseClick, handleItalianClick }) => {
-    const greeting = language === 'Português' ? 'Olá, meu nome é' : language === 'English' ? 'Hello, my name is' : language === '日本語' ? 'こんにちは、私の名前は': 'Ciao, il mio nome è';
+    const greeting = language === 'Português' ? 'Olá, meu nome é' : language === 'English' ? 'Hello, my name is' : language === '日本語' ? 'こんにちは、私の名前は' : 'Ciao, il mio nome è';
     const name = 'Gabriel Sales';
-    const title = language === 'Português' ? 'Engenheiro de Software' : language === 'English' ? 'Software Engineer' : language === '日本語' ? 'ソフトウェアエンジニア': 'Ingegnere del software';
+    const title = language === 'Português' ? 'Engenheiro de Software' : language === 'English' ? 'Software Engineer' : language === '日本語' ? 'ソフトウェアエンジニア' : 'Ingegnere del software';
     const tools = language === 'Português' ? 'Ferramentas' : language === 'English' ? 'Tools' : language === '日本語' ? 'ツール' : 'Strumenti';
     const skills = 'React | Java | JavaScript | PlSQL | SQL | PostGreSQL | Adobe Photoshop | Adobe Illustrator | Sketch | Figma | Canva';
 
@@ -17,11 +17,10 @@ const TelaInicial = ({ language, handlePortugueseClick, handleEnglishClick, hand
                         <div>
                             <nav>
                                 <ul>
-                                    <li><a href="#" onClick={handlePortugueseClick}>🦜Português </a></li>
-                                    <li><a href="#" onClick={handleEnglishClick}>🦅English</a></li>
-                                    <li><a href="#" onClick={handleJapaneseClick}>🐙日本語</a></li>
-                                    <li><a href="#" onClick={handleItalianClick}>🍕Italiano</a></li>
-
+                                    <li><button onClick={handlePortugueseClick}>🦜Português</button></li>
+                                    <li><button onClick={handleEnglishClick}>🦅English</button></li>
+                                    <li><button onClick={handleJapaneseClick}>🐙日本語</button></li>
+                                    <li><button onClick={handleItalianClick}>🍕Italiano</button></li>
                                 </ul>
                             </nav>
                             <div className="Main">
